54c14052a268f5eadd0a866b560587b90cbfa07c
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-07 14:16:55 +0530

    optimize approach

--- customreact.js
+++ customreact.js
@@ -1,12 +1,23 @@
 function mainContainer(reactElement , container) {
     
+    // const domElement = document.createElement(reactElement.type)
+    // domElement.innerHTML = reactElement.children
+    // domElement.setAttribute('href' , reactElement.props.href)
+    // domElement.setAttribute("target" , reactElement.props.target)
+
+    // container.appendChild(domElement)
+
+    /* Lets try to optimize the above code */
+
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
-    domElement.setAttribute('href' , reactElement.props.href)
-    domElement.setAttribute("target" , reactElement.props.target)
 
+    for(const prop in reactElement.props) {
+        if (prop === 'children') continue
+          domElement.setAttribute(prop , reactElement.props[prop])
+    }
     container.appendChild(domElement)
-    
+
 }
 
 const reactElement = {
